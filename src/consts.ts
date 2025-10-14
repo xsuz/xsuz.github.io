c43ca94167091e42560519b060d6ba8f1792fe57
Yuta Suzuki <suzuki7yuta@gmail.com>
2025-10-15 01:38:35 +0900

    AstroのLayoutを適切に使うようにした

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "xsuz's homepage";
-export const SITE_DESCRIPTION = 'xsuzの個人ホームページ';
+export const SITE_TITLE = "xsuz's blog";
+export const SITE_DESCRIPTION = 'すずゆーのブログ';
